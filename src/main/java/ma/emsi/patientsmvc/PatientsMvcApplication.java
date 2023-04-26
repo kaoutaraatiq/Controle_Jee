3410c6107f32285a07442134ddb9502264d11e98
kaoutar <kaoutar.aatiq17@gmail.com>
2023-04-26 12:25:18 +0100

    first commit

---
+++ src/main/java/ma/emsi/patientsmvc/PatientsMvcApplication.java
@@ -0,0 +1,34 @@
+package ma.emsi.patientsmvc;
+
+import ma.emsi.patientsmvc.entities.Patient;
+import ma.emsi.patientsmvc.repositories.PatientRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Date;
+
+@SpringBootApplication
+public class PatientsMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientsMvcApplication.class, args);
+	}
+	//@Bean
+    CommandLineRunner commandLineRunner(PatientRepository patientRepository){
+		return args -> {
+			patientRepository.save(new Patient(null,"Hassan",new Date(),false,122));
+			patientRepository.save(new Patient(null,"Mohammed",new Date(),true,321));
+			patientRepository.save(new Patient(null,"Yasmine",new Date(),true,165));
+			patientRepository.save(new Patient(null,"Hanae",new Date(),false,132));
+
+		};
+}
+@Bean
+        PasswordEncoder passwordEncoder(){
+		      return new BCryptPasswordEncoder();
+		}
+}
